cba966e5af145d18e73823704a1d607180895602
Sarkar-Soumyadeep-202002 <soumyadeep.sarkar@my.jcu.edu.au>
2022-04-06 09:49:24 +0530

    Add display_data function to display the subject records

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,7 +9,7 @@ FILENAME = "subject_data.txt"
 def main():
     """Start the program"""
     data = get_data()
-    print(data)
+    display_data(data)
 
 
 def get_data():
@@ -24,6 +24,12 @@ def get_data():
 
     input_file.close()
     return subject_records
+
+
+def display_data(data):
+    """Display the contents of the file in a proper sentence"""
+    for item in data:
+        print("{} is taught by {:12} and has {:3} students".format(item[0], item[1], item[2]))
 
 
 main()

97b53b2110e9a40dec171907d986e791572c4b7f
Sarkar-Soumyadeep-202002 <soumyadeep.sarkar@my.jcu.edu.au>
2022-04-06 08:58:24 +0530

    Answer the questions

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -24,3 +24,21 @@ numbers[3:4] = [1]
 "3" in numbers : False
 numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 """
+
+"""
+Write Python expressions (in the same Python file) to achieve the following:
+
+1)Change the first element of numbers to "ten" (the string, not the number 10)
+2)Change the last element of numbers to 1
+3)Get all the elements from numbers except the first two (slice)
+4)Check if 9 is an element of numbers
+
+"""
+
+"""
+Answers:
+1) numbers[0] = "ten"
+2) numbers[-1] = 1
+3) numbers[2:]
+4) 9 in numbers
+"""

4e49d25a9d963b75117a28320ce630b3e8201437
Sarkar-Soumyadeep-202002 <soumyadeep.sarkar@my.jcu.edu.au>
2022-04-06 10:07:18 +0530

    Add username function to check if a user has access or not

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -25,3 +25,18 @@ def main():
 
 
 main()
+
+
+def username():
+    """Check if a user can be granted access or not"""
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+    name = input("Enter your username: ")
+    if name in usernames:
+        print("Access granted")
+    else:
+        print("Access Denied")
+
+
+username()

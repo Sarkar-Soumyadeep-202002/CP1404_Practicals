72d9abc381a229049661cf40186f8f357f21a685
Sarkar-Soumyadeep-202002 <soumyadeep.sarkar@my.jcu.edu.au>
2022-04-06 09:41:08 +0530

    Modify the get_data function to return the data

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,6 +7,7 @@ FILENAME = "subject_data.txt"
 
 
 def main():
+    """Start the program"""
     data = get_data()
     print(data)
 
@@ -14,16 +15,15 @@ def main():
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    subject_records = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        subject_records.append(parts)
+
     input_file.close()
+    return subject_records
 
 
 main()

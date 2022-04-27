3604fd2d1433db117341ae034a6f2b5ed4469bfb
Sarkar-Soumyadeep-202002 <soumyadeep.sarkar@my.jcu.edu.au>
2022-04-27 11:53:32 +0800

    Add new object and new field to the Car class

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,13 +4,14 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, fuel, name):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
         self.fuel = fuel
         self.odometer = 0
+        self.name = name
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
@@ -30,3 +31,6 @@ class Car:
         self.odometer += distance
         return distance
 
+    def __str__(self):
+        """Find the formatted string version of the object"""
+        return "{}, fuel={}, odometer={}".format(self.name, self.fuel, self.odometer)
